fcf32770278d1e7e7064226df6f25c43c1718b86
frogSM <frogSM@frogSM-PC>
2015-03-10 13:34:01 +0900

    DB 수정

--- ESC_Project_Server/src/com/esc_project/DatabaseConnection/DBController.java
+++ ESC_Project_Server/src/com/esc_project/DatabaseConnection/DBController.java
@@ -8,34 +8,32 @@ import java.sql.Statement;
 
 public class DBController {
 	/**
-	 *  - DBController 클래스 - *
-	 *  
-	 * 1. 데이터베이스를 접속 
-	 * 2. 명령어별로 다르게 수행되는 메소드 구현
-	 * 3. 명령어별로 적합한 객체 형태로 반환
+	 * - DBController 클래스 - *
+	 * 
+	 * 1. 데이터베이스를 접속 2. 명령어별로 다르게 수행되는 메소드 구현 3. 명령어별로 적합한 객체 형태로 반환
 	 * 
 	 * **/
 
 	/** 서버 MySql 정보 **/
 	private final String driverName = "com.mysql.jdbc.Driver";
 	private final String DBName = "test";
-	private final String dbURL = "jdbc:mysql://localhost/"+DBName;
-	
+	private final String dbURL = "jdbc:mysql://localhost/" + DBName;
+
 	/** DB 테이블 선언 **/
 	private Product mProduct;
-	
+
 	/** MySql Connection 지역변수 **/
 	private Connection con;
 	private Statement stmt;
 	private ResultSet result;
-	
+
 	public DBController() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	/** 외부 DB인 MySql에 접속 **/
 	private void connectDB() {
-		
+
 		try {
 			Class.forName(driverName);
 			con = DriverManager.getConnection(dbURL, "root", "apmsetup");
@@ -49,10 +47,10 @@ public class DBController {
 		}
 		System.out.println("Mysql DB Connection");
 	}
-	
+
 	/** 외부 DB인 MySql과 접속 해제 **/
 	private void closeDB() {
-		
+
 		try {
 			con.close();
 		} catch (SQLException e) {
@@ -61,29 +59,32 @@ public class DBController {
 		}
 		System.out.println("Mysql DB Close");
 	}
-	
+
 	/** 명령어 Constants.Uid_Info의 수행 동작 **/
 	public Product Uid_Info(String uid) {
-		
-		String SQL ;
-		
+
+		String SQL;
+
 		try {
-			
+
 			connectDB();
-			
+
 			SQL = "select * from product where uid=\"" + uid + "\"";
 			result = stmt.executeQuery(SQL);
-			while(result.next()) {
-				mProduct = new Product(result.getString("name"), result.getString("price"), result.getString("type"), result.getString("x"), result.getString("y"));
+			while (result.next()) {
+				mProduct = new Product(result.getString("name"),
+						result.getString("price"), result.getString("type"),
+						result.getString("x"), result.getString("y"), 
+						result.getString("description"), result.getString("manufacturer"));
 			}
-			
+
 			closeDB();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 		return mProduct;
 	}
-	
+
 }
--- ESC_Project_Server/src/com/esc_project/DatabaseConnection/Product.java
+++ ESC_Project_Server/src/com/esc_project/DatabaseConnection/Product.java
@@ -7,14 +7,18 @@ public class Product {
 	String type;
 	String x;
 	String y;
+	String description;
+	String manufacturer;
 	
-	public Product( String name, String price, String type, String x, String y) {
+	public Product( String name, String price, String type, String x, String y, String description, String manufacturer) {
 		// TODO Auto-generated constructor stub
 		this.name = name;
 		this.price = price;
 		this.type = type;
 		this.x = x;
 		this.y = y;
+		this.description = description;
+		this.manufacturer = manufacturer;
 	}
 	
 	public void setName(String name) {
@@ -30,21 +34,40 @@ public class Product {
 		this.y = y;
 	}
 	
+	public void setDescription( String description ) {
+		this.description = description;
+	}
+	
+	public void setManufacturer ( String manufacturer ) {
+		this.manufacturer = manufacturer;
+	}
+	
 
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getPrice() {
 		return price;
 	}
+
 	public String getType( ) { 
 		return type;
 	}
+	
 	public String getX() { 
 		return x;
 	}
+	
 	public String getY() { 
 		return y;
+	}
+	
+	public String getDescription( ) {
+		return this.description;
+	}
+	
+	public String getManufacturer ( ) {
+		return this.manufacturer;
 	}
 }
