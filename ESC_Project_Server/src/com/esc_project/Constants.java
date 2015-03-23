53a3fe7e7aee4d0420398ee01c825b60b24b7355
Administrator <Administrator@MSDN-SPECIAL>
2015-03-23 20:25:20 +0900

    최신버젼

--- ESC_Project_Server/src/com/esc_project/Constants.java
+++ ESC_Project_Server/src/com/esc_project/Constants.java
@@ -5,4 +5,6 @@ public class Constants {
 	/**  JsonHelper & ClientController Constants **/
 	public final static String Uid_Info = "Uid_Info";
 	public final static String All_Product_Info = "All_Product_Info";
+	public final static String NoticeTitleList = "NoticeTitleList";
+	public final static String NoticeContent = "NoticeContent";
 }
